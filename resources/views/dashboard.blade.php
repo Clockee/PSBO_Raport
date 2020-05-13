26ac74ba5c1c010e4deb8cb21dbfd804fc3d0ce1
Fakhor <fakhoor_fafa@apps.ipb.ac.id>
2020-05-13 23:46:25 +0700

    2

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<!-- Using Templating Engine Blade Laravel -->
+<!-- Import from resources/views/layout/main.blade.php -->
+@extends('layout/main')
+
+@section('title', 'Dashboard')
+
+@section('container')
+
+@if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+@endif
+
+
+@endsection
